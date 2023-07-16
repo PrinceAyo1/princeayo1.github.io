c698779215f03d79170395b606fbd0ff0c6ea275
PrinceAyo1 <44818424+PrinceAyo1@users.noreply.github.com>
2023-07-16 22:15:50 +0100

    Amend code
    I have looked into the carousels I created and amended the CSS and JS code to function with chevrons

--- script.js
+++ script.js
@@ -1,31 +1,57 @@
-// script.js
-
 document.addEventListener("DOMContentLoaded", function () {
-  const interestsCarousel = document.getElementById("interests-carousel");
-  const interestsImages = Array.from(
-    interestsCarousel.getElementsByTagName("img")
-  );
-  let interestsIndex = 0;
+  const carousel = document.getElementById("interests-carousel");
+  const images = carousel.querySelectorAll("img");
+  const numImages = images.length;
+  let currentIndex = 0;
 
-  function showNextInterest() {
-    interestsImages.forEach((image) => (image.style.opacity = 0));
-    interestsImages[interestsIndex].style.opacity = 1;
-    interestsIndex = (interestsIndex + 1) % interestsImages.length;
+  function showImage(index) {
+    images.forEach((img, idx) => {
+      img.style.display = idx === index ? "block" : "none";
+    });
   }
 
-  setInterval(showNextInterest, 2000);
-
-  const inspirationCarousel = document.getElementById("inspiration-carousel");
-  const inspirationWords = Array.from(
-    inspirationCarousel.getElementsByTagName("span")
-  );
-  let inspirationIndex = 0;
-
-  function showNextWord() {
-    inspirationWords.forEach((word) => (word.style.opacity = 0));
-    inspirationWords[inspirationIndex].style.opacity = 1;
-    inspirationIndex = (inspirationIndex + 1) % inspirationWords.length;
+  function nextImage() {
+    currentIndex = (currentIndex + 1) % numImages;
+    showImage(currentIndex);
   }
+
+  function prevImage() {
+    currentIndex = (currentIndex - 1 + numImages) % numImages;
+    showImage(currentIndex);
+  }
+
+  // Initial setup
+  showImage(currentIndex);
+
+  // Automatic sliding (change slide every 3 seconds)
+  setInterval(nextImage, 3000);
+
+  // Handle click events for navigation
+  document.getElementById("prevBtn").addEventListener("click", prevImage);
+  document.getElementById("nextBtn").addEventListener("click", nextImage);
 
   setInterval(showNextWord, 2000);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const carousel = document.getElementById("inspiration-carousel");
+  const words = carousel.querySelectorAll("span");
+  let currentIndex = 0;
+
+  function showWord(index) {
+    words.forEach((word, idx) => {
+      word.style.display = idx === index ? "inline-block" : "none";
+    });
+  }
+
+  function nextWord() {
+    currentIndex = (currentIndex + 1) % words.length;
+    showWord(currentIndex);
+  }
+
+  // Initial setup
+  showWord(currentIndex);
+
+  // Flashing effect (change word every 2 seconds)
+  setInterval(nextWord, 5000);
 });
